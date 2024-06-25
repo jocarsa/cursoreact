c94c1daa75bc7d9ea76ae05aa4b62d9b272f5861
= <info@josevicentecarratala.com>
2024-06-25 15:13:12 +0200

    1

---
+++ 001-Interfaz/002-Importacion de componente/Perfil.js
@@ -0,0 +1,8 @@
+export default function Perfil() {
+  return (
+    <img
+      src="https://pbs.twimg.com/profile_images/1234182602027868160/_QH5i5tz_400x400.jpg"
+      alt="Jose Vicente Carratala"
+    />
+  );
+}
